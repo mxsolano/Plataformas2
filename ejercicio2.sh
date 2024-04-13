53abdfc8b3b9b78d703c12b4705cb8c0099b50ee
Mery Solano <166348748+mxsolano@users.noreply.github.com>
2024-04-13 14:26:00 -0600

    monitoreo del proceso ingresado

---
+++ ejercicio2.sh
@@ -0,0 +1,16 @@
+!/bin/bash
+
+#guardo el nombre del proceso y el comando a ejecutar
+nombre_proceso="$1"
+comando="$2"
+
+#para monitorear el proceso recibido
+monitoreo_proceso(){
+  if pgrep -x "$nombre_proceso" > /dev/null; then
+    echo "el proceso esta corriendo"
+  else
+    echo "EL proceso no esta corriendo"
+
+    $comando & echo "Proceso $nombre_proceso inciado"
+  fi
+}
